6e54cee0105088c650b31160eb2bf776807f2b5e
Sev23456 <162345015+Sev23456@users.noreply.github.com>
2026-04-12 23:33:35 +0300

    Delete frog_lib directory

--- frog_lib/metabolism/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Системы метаболизма"""
-
-from .systemic_metabolism import SystemicMetabolism, NeuronMetabolism
-
-__all__ = [
-    'SystemicMetabolism',
-    'NeuronMetabolism',
-]
